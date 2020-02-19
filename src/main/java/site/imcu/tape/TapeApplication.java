c9df24488b4f48c79de50bf9bb3a037b561b8bf3
menghe <menghehe@live.cn>
2020-02-19 18:26:00 +0800

    毕业设计项目tap服务端初始化

---
+++ src/main/java/site/imcu/tape/TapeApplication.java
@@ -0,0 +1,13 @@
+package site.imcu.tape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapeApplication.class, args);
+	}
+
+}
